951dc34561a5f74b3154fbb831f5f6e3405e8a2b
Jiyeon <yeeoni_@naver.com>
2021-12-31 02:22:48 +0900

    [Feat] scroll 하면 header 배경색 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { motion } from 'framer-motion';
-import React, { useState } from 'react';
+import { motion, useAnimation, useViewportScroll } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import '@fortawesome/fontawesome-free/js/all.js';
 import { Link } from 'react-router-dom';
@@ -30,10 +30,11 @@ const Logo = styled.div`
   }
 `;
 
-const Nav = styled.ul`
+const Nav = styled.ul<{ navTextColor: string }>`
   width: 40%;
   display: flex;
   justify-content: space-between;
+  color: ${(props) => props.navTextColor};
 `;
 
 const Item = styled.li`
@@ -46,14 +47,14 @@ const Item = styled.li`
   }
 `;
 
-const AllMenuBtn = styled.button`
+const AllMenuBtn = styled.button<{ navTextColor: string }>`
   border: 0;
   outline: none;
   background-color: transparent;
   cursor: pointer;
   svg {
     font-size: 18px;
-    color: white;
+    color: ${(props) => props.navTextColor};
     transition: color 150ms;
     &:hover {
       color: ${(props) => props.theme.mainColor};
@@ -61,16 +62,48 @@ const AllMenuBtn = styled.button`
   }
 `;
 
+const navVariants = {
+  top: {
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    transition: {
+      type: 'tween',
+      duration: 0,
+    },
+  },
+  scroll: {
+    backgroundColor: 'rgba(255,255,255,1)',
+    transition: {
+      type: 'tween',
+      duration: 0,
+    },
+  },
+};
+
 const Header = () => {
   const [openAllMenu, setOpenAllMenu] = useState(false);
+  const [navTextColor, setNavTextColor] = useState<string>('white');
+  const { scrollY } = useViewportScroll();
+  const navAnimation = useAnimation();
+
+  useEffect(() => {
+    scrollY.onChange(() => {
+      if (scrollY.get() > 160) {
+        navAnimation.start('scroll');
+        setNavTextColor('black');
+      } else {
+        navAnimation.start('top');
+        setNavTextColor('white');
+      }
+    });
+  }, [scrollY, navAnimation]);
 
   return (
-    <Wrapper>
+    <Wrapper variants={navVariants} animate={navAnimation} initial={'top'}>
       <Layout>
         <Logo>
           <img src="logo.png" alt="" />
         </Logo>
-        <Nav>
+        <Nav navTextColor={navTextColor}>
           <Item>
             <Link to="/">im1l coffee</Link>
           </Item>
@@ -87,7 +120,10 @@ const Header = () => {
             <Link to="/">news</Link>
           </Item>
         </Nav>
-        <AllMenuBtn onClick={() => setOpenAllMenu(true)}>
+        <AllMenuBtn
+          navTextColor={navTextColor}
+          onClick={() => setOpenAllMenu(true)}
+        >
           <i className="fas fa-bars"></i>
         </AllMenuBtn>
       </Layout>
