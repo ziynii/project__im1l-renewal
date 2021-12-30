1c7a009d72a44f328e7e01f6e9268e5869406ba2
Jiyeon <yeeoni_@naver.com>
2021-12-31 02:23:38 +0900

    [Feat] Home의 메인배너와 메뉴리스트 추가

---
+++ src/components/MenuCard.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+import { IMenu } from '../data';
+
+interface IMenuCardProps {
+  menu: IMenu;
+}
+
+const Item = styled.li`
+  position: relative;
+  height: 350px;
+  color: ${(props) => props.theme.textColor};
+  text-align: center;
+  /* border: 1px solid lightgray; */
+  border-radius: 16px;
+  box-shadow: 0px 10px 13px -7px lightgray, 5px 5px 15px 5px rgba(0, 0, 0, 0);
+  z-index: -1;
+  &:not(:last-child) {
+    margin-right: 0.8em;
+  }
+  img {
+    width: 200px;
+		border-radius: 16px;
+  }
+`;
+
+const TextWrap = styled.div`
+  width: 100%;
+  position: absolute;
+  bottom: 12px;
+  left: 50%;
+  transform: translateX(-50%);
+  h4 {
+    font-weight: bold;
+  }
+  p {
+    font-size: 0.7em;
+    margin-top: 2px;
+  }
+`;
+
+const MenuCard = ({ menu }: IMenuCardProps) => {
+  return (
+    <Item>
+      <img src={menu.url} alt={menu.name} />
+      <TextWrap>
+        <h4>{menu.name}</h4>
+        <p>{menu.option}</p>
+      </TextWrap>
+    </Item>
+  );
+};
+
+export default MenuCard;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,179 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import MenuCard from '../components/MenuCard';
+import { IMenu } from '../data';
 
-const Home = () => {
-  return null;
+interface IHomeProps {
+  menus: IMenu[];
+}
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100%;
+`;
+
+const MainBanner = styled.div`
+  width: 100%;
+  height: 700px;
+  background-image: url('image/bg.jpg');
+  background-size: cover;
+  background-position: center center;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const MainTitle = styled.div`
+  margin-left: 120px;
+  p {
+    font-size: 20px;
+    font-weight: 400;
+  }
+  h2 {
+    margin-top: 28px;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+  span {
+    font-weight: 500;
+    font-size: 36px;
+  }
+`;
+
+const ContentWrap = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 1.5em 2em;
+`;
+
+const ContnetTitle = styled.h2`
+  text-transform: uppercase;
+  text-align: center;
+  color: ${(props) => props.theme.textColor};
+  font-weight: 500;
+  font-size: 2em;
+`;
+
+const ContentCaption = styled.p`
+  text-align: center;
+  font-size: 0.9em;
+  color: gray;
+  margin-top: 0.4em;
+  margin-bottom: 1.6em;
+`;
+
+const MenuBtns = styled.div<{ menuTabNum: number }>`
+  text-align: center;
+  button {
+    outline: none;
+    border: 0;
+    cursor: pointer;
+    background-color: transparent;
+    padding: 0.4em 0.8em;
+    text-transform: uppercase;
+    font-size: 0.8em;
+    transition: color 150ms;
+    &:hover {
+      color: ${(props) => props.theme.mainColor};
+    }
+    &:nth-child(${(props) => props.menuTabNum}) {
+      color: ${(props) => props.theme.mainColor};
+    }
+    &:not(:last-child) {
+      border-right: 1px solid lightgray;
+    }
+  }
+`;
+
+const MenuItems = styled.ul`
+  height: 430px;
+  display: flex;
+  align-items: center;
+  overflow-x: auto;
+  &::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme.mainColor};
+    border-radius: 16px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: rgba(0, 0, 0, 0.1);
+    border-radius: 16px;
+  }
+  &::-webkit-scrollbar {
+    height: 6px;
+  }
+`;
+
+const Home = ({ menus }: IHomeProps) => {
+  const [menuSelect, setMenuSelect] = useState<string>('coffee');
+  const [menuTabNum, setMenuTabNum] = useState<number>(1);
+  const filterMenu = menus.filter((menu) => {
+    return menu.category === menuSelect;
+  });
+
+  console.log(filterMenu);
+
+  return (
+    <Wrapper>
+      <MainBanner>
+        <MainTitle>
+          <p>IM1L COFFEE</p>
+          <h2>
+            고객이 인정한 커피전문점, <br />
+            <span>아임일리터</span>
+          </h2>
+        </MainTitle>
+      </MainBanner>
+      <ContentWrap>
+        <ContnetTitle>im1l menu</ContnetTitle>
+        <ContentCaption>
+          고객을 위한 정성이 담긴 아임일리터의 메뉴입니다
+        </ContentCaption>
+        <MenuBtns menuTabNum={menuTabNum}>
+          <button
+            onClick={() => {
+              setMenuSelect('coffee');
+              setMenuTabNum(1);
+            }}
+          >
+            coffee
+          </button>
+          <button
+            onClick={() => {
+              setMenuSelect('beverage');
+              setMenuTabNum(2);
+            }}
+          >
+            beverage
+          </button>
+          <button
+            onClick={() => {
+              setMenuSelect('tea');
+              setMenuTabNum(3);
+            }}
+          >
+            tea
+          </button>
+        </MenuBtns>
+        <MenuItems>
+          {filterMenu.map((menu) => (
+            <MenuCard key={menu.id} menu={menu} />
+          ))}
+        </MenuItems>
+      </ContentWrap>
+      <ContentWrap>
+        <ContnetTitle>merchandiser</ContnetTitle>
+        <ContentCaption>
+          다양한 상품들로 언제 어디서나 아임일리터와 함께하세요.
+        </ContentCaption>
+      </ContentWrap>
+      <ContentWrap>
+        <ContnetTitle>franchise</ContnetTitle>
+        <ContentCaption>
+          함께 성장하는 아임일리터와 창업성공의 꿈을 이루세요.
+        </ContentCaption>
+      </ContentWrap>
+    </Wrapper>
+  );
 };
 
 export default Home;
