be2377acfcd21af9f4ad08b43b69e3799561bac2
Jiyeon <yeeoni_@naver.com>
2022-01-02 23:13:46 +0900

    [Design] MenuCard hover시 css 추가

--- src/components/Home/MenuCard.tsx
+++ src/components/Home/MenuCard.tsx
@@ -13,13 +13,44 @@ const Item = styled.li`
   text-align: center;
   border-radius: 16px;
   box-shadow: 0px 10px 13px -7px lightgray, 5px 5px 15px 5px rgba(0, 0, 0, 0);
-  z-index: -1;
   &:not(:last-child) {
     margin-right: 1.2em;
   }
   img {
     width: 200px;
-		border-radius: 16px;
+    border-radius: 16px;
+  }
+  .hover__content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+		padding-bottom: 24px;
+    background: linear-gradient(transparent 10%, rgba(237, 164, 131, 1) 90%);
+    opacity: 0;
+    transition: opacity 150ms ease-in;
+    .go {
+      color: white;
+      font-size: 0.9em;
+      padding: 4px 8px;
+      outline: none;
+      border: 1px solid white;
+      background-color: transparent;
+      transition: all 150ms;
+      cursor: pointer;
+      &:hover {
+        background-color: white;
+        color: ${(props) => props.theme.mainColor};
+      }
+    }
+  }
+  &:hover .hover__content {
+    opacity: 1;
   }
 `;
 
@@ -46,6 +77,9 @@ const MenuCard = ({ menu }: IMenuCardProps) => {
         <h4>{menu.name}</h4>
         <p>{menu.option || menu.desciption}</p>
       </TextWrap>
+      <div className="hover__content">
+        <button className="go">자세히 보기</button>
+      </div>
     </Item>
   );
 };
