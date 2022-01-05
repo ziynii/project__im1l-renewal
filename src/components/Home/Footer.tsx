8cc297edef24c8e39b5e157457e159c45182d565
Jiyeon <yeeoni_@naver.com>
2022-01-05 12:32:03 +0900

    [Refactor] Footer 컴포넌트 분리

---
+++ src/components/Home/Footer.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.footer`
+  padding: 3.6em 0;
+  margin-top: 4.8em;
+  background-color: ${(props) => props.theme.textColor};
+  .layout {
+    max-width: 1280px;
+    margin: 0 auto;
+    img {
+      width: 180px;
+      margin-bottom: 1.2em;
+    }
+    p {
+      margin-top: 0.8rem;
+      font-size: 0.9em;
+    }
+  }
+`;
+
+const Snsicon = styled.div`
+  display: flex;
+  margin-top: 1.2rem;
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    border: 1px solid lightgray;
+    border-radius: 50%;
+    color: lightgray;
+    transition: all 150ms;
+    &:hover {
+      color: ${(props) => props.theme.mainColor};
+      border: 1px solid ${(props) => props.theme.mainColor};
+    }
+    &:not(:last-child) {
+      margin-right: 12px;
+    }
+  }
+  svg {
+    font-size: 18px;
+  }
+`;
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      <div className="layout">
+        <img src="/image/logo-full-white.png" alt="흰색 글씨 로고" />
+        <p>법인명 : (주)아임일리터 | 대표자 : 임시훈</p>
+        <p>주소지 : 대전광역시 중구 대종로 451, 3층 </p>
+        <p>
+          TEL : 1600-1061 | FAX : 042-252-1126 | Email : im1lcoffee@naver.com{' '}
+        </p>
+        <Snsicon>
+          <a href="#">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-youtube"></i>
+          </a>
+          <a href="#">
+            <i className="fas fa-blog"></i>
+          </a>
+        </Snsicon>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Footer;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,9 @@
-import React, { DragEvent, MouseEvent, useRef, useState } from 'react';
+import React, { MouseEvent, useRef, useState } from 'react';
 import styled from 'styled-components';
 import MenuCard from '../components/Home/MenuCard';
 import { IMenu } from '../data';
 import '@fortawesome/fontawesome-free/js/all.js';
+import Footer from '../components/Home/Footer';
 
 interface IHomeProps {
   menus: IMenu[];
@@ -210,50 +211,6 @@ const CardText = styled.div`
   }
 `;
 
-const Footer = styled.footer`
-  padding: 3.6em 0;
-  margin-top: 4.8em;
-  background-color: ${(props) => props.theme.textColor};
-  .layout {
-    max-width: 1280px;
-    margin: 0 auto;
-    img {
-      width: 180px;
-      margin-bottom: 1.2em;
-    }
-    p {
-      margin-top: 0.8rem;
-      font-size: 0.9em;
-    }
-  }
-`;
-
-const Snsicon = styled.div`
-  display: flex;
-  margin-top: 1.2rem;
-  a {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 40px;
-    height: 40px;
-    border: 1px solid lightgray;
-    border-radius: 50%;
-    color: lightgray;
-    transition: all 150ms;
-    &:hover {
-      color: ${(props) => props.theme.mainColor};
-      border: 1px solid ${(props) => props.theme.mainColor};
-    }
-    &:not(:last-child) {
-      margin-right: 12px;
-    }
-  }
-  svg {
-    font-size: 18px;
-  }
-`;
-
 const Home = ({ menus }: IHomeProps) => {
   const [menuSelect, setMenuSelect] = useState<string>('coffee');
   const [menuTabNum, setMenuTabNum] = useState<number>(1);
@@ -448,27 +405,7 @@ const Home = ({ menus }: IHomeProps) => {
           </Card>
         </Franchise>
       </ContentWrap>
-      <Footer>
-        <div className="layout">
-          <img src="image/logo-full-white.png" alt="흰색 글씨 로고" />
-          <p>법인명 : (주)아임일리터 | 대표자 : 임시훈</p>
-          <p>주소지 : 대전광역시 중구 대종로 451, 3층 </p>
-          <p>
-            TEL : 1600-1061 | FAX : 042-252-1126 | Email : im1lcoffee@naver.com{' '}
-          </p>
-          <Snsicon>
-            <a href="#">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-youtube"></i>
-            </a>
-            <a href="#">
-              <i className="fas fa-blog"></i>
-            </a>
-          </Snsicon>
-        </div>
-      </Footer>
+      <Footer />
     </Wrapper>
   );
 };
